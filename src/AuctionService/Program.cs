72b78cbb523a99ac3c8c44e48ac9c14140753991
Andrew Virts <asvirts@outlook.com>
2023-12-09 14:32:31 -0600

    creating seed data

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -5,7 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
-builder.Services.AddDbContext<AuctionDbContext>(opt => {
+builder.Services.AddDbContext<AuctionDbContext>(opt =>
+{
     opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
@@ -16,5 +17,14 @@ var app = builder.Build();
 app.UseAuthorization();
 
 app.MapControllers();
+
+try
+{
+    Dbinitializer.InitDb(app);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
 
 app.Run();
