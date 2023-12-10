953a19e349e96fea96ad32bb57d11a3810f8d0eb
Andrew Virts <asvirts@outlook.com>
2023-12-09 20:46:17 -0600

    docker compose mongodb

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,3 +1,8 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Data;
+using SearchService.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,5 +13,14 @@ var app = builder.Build();
 app.UseAuthorization();
 
 app.MapControllers();
+
+try
+{
+    await DbInitializer.InitDb(app);
+}
+catch (System.Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
 
 app.Run();
